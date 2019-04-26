1128eeff65896027f4b7f81a8af39afceb536690
Jiakai Liu <liujiakai@fb.com>
2019-04-25 22:49:31 -0700

    replace caffe2 with libtorch

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,58 +1,84 @@
 #include <jni.h>
 #include <string>
+#include <sstream>
+#include <fstream>
+#include <memory>
+#include <vector>
 #include <algorithm>
-#define PROTOBUF_USE_DLLS 1
-#define CAFFE2_USE_LITE_PROTO 1
-#include <caffe2/core/predictor.h>
-#include <caffe2/core/operator.h>
-#include <caffe2/core/timer.h>
-
-#include "caffe2/core/init.h"
-
+#include <time.h>
 #include <android/asset_manager.h>
 #include <android/asset_manager_jni.h>
 #include <android/log.h>
+#include <chrono>
+#include "torch/script.h"
+
+#define PROTOBUF_USE_DLLS 1
+#define CAFFE2_USE_LITE_PROTO 1
+
 #include "classes.h"
+
 #define IMG_H 227
 #define IMG_W 227
 #define IMG_C 3
 #define MAX_DATA_SIZE IMG_H * IMG_W * IMG_C
-#define alog(...) __android_log_print(ANDROID_LOG_ERROR, "F8DEMO", __VA_ARGS__);
 
-static caffe2::NetDef _initNet, _predictNet;
-static caffe2::Predictor *_predictor;
-static char raw_data[MAX_DATA_SIZE];
+#define alog(...) __android_log_print(ANDROID_LOG_ERROR, "TORCH", __VA_ARGS__);
+#define min(a,b) ((a) > (b)) ? (b) : (a)
+#define max(a,b) ((a) > (b)) ? (a) : (b)
+
+static std::string storage_dir;
+static std::string debug_file_prefix;
 static float input_data[MAX_DATA_SIZE];
-static caffe2::Workspace ws;
+static std::shared_ptr<torch::jit::script::Module> module;
+static at::Tensor input{torch::zeros({1, IMG_C, IMG_H, IMG_W})};
+static int debug_counter;
 
-// A function to load the NetDefs from protobufs.
-void loadToNetDef(AAssetManager* mgr, caffe2::NetDef* net, const char *filename) {
-    AAsset* asset = AAssetManager_open(mgr, filename, AASSET_MODE_BUFFER);
-    assert(asset != nullptr);
-    const void *data = AAsset_getBuffer(asset);
-    assert(data != nullptr);
-    off_t len = AAsset_getLength(asset);
-    assert(len != 0);
-    if (!net->ParseFromArray(data, len)) {
-        alog("Couldn't parse net from data.\n");
-    }
-    AAsset_close(asset);
+extern "C" JNIEXPORT void JNICALL
+Java_facebook_f8demo_ClassifyCamera_setDebugDirectory(
+        JNIEnv* env,
+        jobject /* this */,
+        jstring directory) {
+    const char *dirString = env->GetStringUTFChars(directory, 0);
+    storage_dir = std::string(dirString);
+    debug_file_prefix = storage_dir + "/" + "debug";
+    //std::ofstream out(std::string(dirString) + "/" + "debug.txt");
+    env->ReleaseStringUTFChars(directory, dirString);
 }
 
-extern "C"
-void
-Java_facebook_f8demo_ClassifyCamera_initCaffe2(
+extern "C" JNIEXPORT void JNICALL
+Java_facebook_f8demo_ClassifyCamera_initModel(
         JNIEnv* env,
         jobject /* this */,
         jobject assetManager) {
+#if 0
     AAssetManager *mgr = AAssetManager_fromJava(env, assetManager);
-    alog("Attempting to load protobuf netdefs...");
-    loadToNetDef(mgr, &_initNet,   "squeeze_init_net.pb");
-    loadToNetDef(mgr, &_predictNet,"squeeze_predict_net.pb");
-    alog("done.");
-    alog("Instantiating predictor...");
-    _predictor = new caffe2::Predictor(_initNet, _predictNet);
-    alog("done.")
+
+    std::string filename = "res18.pb";
+    std::string data;
+
+    AAsset* asset = AAssetManager_open(mgr, filename.c_str(), AASSET_MODE_STREAMING);
+    char buf[1024];
+    int nb_read = 0;
+    while ((nb_read = AAsset_read(asset, buf, 1024)) > 0) {
+        data.append(buf, nb_read);
+    }
+    AAsset_close(asset);
+
+    std::istringstream input{data};
+    module = torch::jit::load(input);
+
+    std::vector<torch::jit::IValue> inputs;
+    inputs.push_back(torch::ones({1, IMG_C, IMG_H, IMG_W}));
+    at::Tensor output = module->forward(inputs).toTensor();
+
+    std::ostringstream out;
+    out << "ResNet-18" << std::endl;
+    out << output.slice(/*dim=*/1, /*start=*/0, /*end=*/5) << std::endl;
+
+    return env->NewStringUTF(out.str().c_str());
+#endif
+    std::ifstream input(storage_dir + "/squeeze.model");
+    module = torch::jit::load(input);
 }
 
 float avg_fps = 0.0;
@@ -61,88 +87,70 @@ int iters_fps = 10;
 
 extern "C"
 JNIEXPORT jstring JNICALL
-Java_facebook_f8demo_ClassifyCamera_classificationFromCaffe2(
+Java_facebook_f8demo_ClassifyCamera_classification(
         JNIEnv *env,
         jobject /* this */,
         jint h, jint w, jbyteArray Y, jbyteArray U, jbyteArray V,
         jint rowStride, jint pixelStride,
         jboolean infer_HWC) {
-    if (!_predictor) {
+    if (!module) {
         return env->NewStringUTF("Loading...");
     }
-    jsize Y_len = env->GetArrayLength(Y);
     jbyte * Y_data = env->GetByteArrayElements(Y, 0);
-    assert(Y_len <= MAX_DATA_SIZE);
-    jsize U_len = env->GetArrayLength(U);
     jbyte * U_data = env->GetByteArrayElements(U, 0);
-    assert(U_len <= MAX_DATA_SIZE);
-    jsize V_len = env->GetArrayLength(V);
     jbyte * V_data = env->GetByteArrayElements(V, 0);
-    assert(V_len <= MAX_DATA_SIZE);
-
-#define min(a,b) ((a) > (b)) ? (b) : (a)
-#define max(a,b) ((a) > (b)) ? (a) : (b)
 
     auto h_offset = max(0, (h - IMG_H) / 2);
     auto w_offset = max(0, (w - IMG_W) / 2);
 
-    auto iter_h = IMG_H;
-    auto iter_w = IMG_W;
-    if (h < IMG_H) {
-        iter_h = h;
-    }
-    if (w < IMG_W) {
-        iter_w = w;
-    }
+    auto iter_h = min(IMG_H, h);
+    auto iter_w = min(IMG_W, w);
 
     for (auto i = 0; i < iter_h; ++i) {
-        jbyte* Y_row = &Y_data[(h_offset + i) * w];
-        jbyte* U_row = &U_data[(h_offset + i) / 2 * rowStride];
-        jbyte* V_row = &V_data[(h_offset + i) / 2 * rowStride];
+        jbyte* Y_row = &Y_data[(h_offset + i) * rowStride];
+        jbyte* U_row = &U_data[(h_offset + i)/2 * rowStride];
+        jbyte* V_row = &V_data[(h_offset + i)/2 * rowStride];
         for (auto j = 0; j < iter_w; ++j) {
-            // Tested on Pixel and S7.
-            char y = Y_row[w_offset + j];
-            char u = U_row[pixelStride * ((w_offset+j)/pixelStride)];
-            char v = V_row[pixelStride * ((w_offset+j)/pixelStride)];
+            char y = Y_row[(w_offset+j)];
+            char u = U_row[pixelStride * ((w_offset+j) / 2)];
+            char v = V_row[pixelStride * ((w_offset+j) / 2)];
 
-            float b_mean = 104.00698793f;
-            float g_mean = 116.66876762f;
-            float r_mean = 122.67891434f;
-
-            auto b_i = 0 * IMG_H * IMG_W + j * IMG_W + i;
+            auto b_i = 2 * IMG_H * IMG_W + j * IMG_W + i;
             auto g_i = 1 * IMG_H * IMG_W + j * IMG_W + i;
-            auto r_i = 2 * IMG_H * IMG_W + j * IMG_W + i;
+            auto r_i = 0 * IMG_H * IMG_W + j * IMG_W + i;
 
-            if (infer_HWC) {
-                b_i = (j * IMG_W + i) * IMG_C;
-                g_i = (j * IMG_W + i) * IMG_C + 1;
-                r_i = (j * IMG_W + i) * IMG_C + 2;
-            }
-/*
-  R = Y + 1.402 (V-128)
-  G = Y - 0.34414 (U-128) - 0.71414 (V-128)
-  B = Y + 1.772 (U-V)
- */
-            input_data[r_i] = -r_mean + (float) ((float) min(255., max(0., (float) (y + 1.402 * (v - 128)))));
-            input_data[g_i] = -g_mean + (float) ((float) min(255., max(0., (float) (y - 0.34414 * (u - 128) - 0.71414 * (v - 128)))));
-            input_data[b_i] = -b_mean + (float) ((float) min(255., max(0., (float) (y + 1.772 * (u - v)))));
-
+            input_data[r_i] = (float) ((float) min(255., max(0., (float) (y + 1.402 * (v - 128))))) / 255.;
+            input_data[g_i] = (float) ((float) min(255., max(0., (float) (y - 0.34414 * (u - 128) - 0.71414 * (v - 128))))) / 255.;
+            input_data[b_i] = (float) ((float) min(255., max(0., (float) (y + 1.772 * (u - v))))) / 255.;
         }
     }
 
-    caffe2::TensorCPU input;
-    if (infer_HWC) {
-        input.Resize(std::vector<int>({IMG_H, IMG_W, IMG_C}));
-    } else {
-        input.Resize(std::vector<int>({1, IMG_C, IMG_H, IMG_W}));
+    memcpy(input.data<float>(), input_data, IMG_H * IMG_W * IMG_C * sizeof(float));
+
+#if 0
+    {
+        alog("Debug: %d", debug_counter);
+        if (debug_counter++ % 10 == 0) {
+            std::ostringstream filename;
+            filename << debug_file_prefix << debug_counter;
+            std::ofstream out(filename.str());
+            alog("Debug file: %s", filename.str().c_str());
+            auto data_ptr = input.data<float>();
+            for (int i = 0; i < IMG_H * IMG_W * IMG_C; i++) {
+                out << data_ptr[i] << " ";
+            }
+        }
     }
-    memcpy(input.mutable_data<float>(), input_data, IMG_H * IMG_W * IMG_C * sizeof(float));
-    caffe2::Predictor::TensorVector input_vec{&input};
-    caffe2::Predictor::TensorVector output_vec;
-    caffe2::Timer t;
-    t.Start();
-    _predictor->run(input_vec, &output_vec);
-    float fps = 1000/t.MilliSeconds();
+#endif
+
+    std::vector<torch::jit::IValue> inputs;
+    inputs.push_back(input);
+
+    auto start = std::chrono::system_clock::now();
+    at::Tensor output = module->forward(inputs).toTensor();
+    auto end = std::chrono::system_clock::now();
+
+    float fps = 1000 / std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
     total_fps += fps;
     avg_fps = total_fps / iters_fps;
     total_fps -= avg_fps;
@@ -151,29 +159,28 @@ Java_facebook_f8demo_ClassifyCamera_classificationFromCaffe2(
     float max[k] = {0};
     int max_index[k] = {0};
     // Find the top-k results manually.
-    if (output_vec.capacity() > 0) {
-        for (auto output : output_vec) {
-            for (auto i = 0; i < output->size(); ++i) {
-                for (auto j = 0; j < k; ++j) {
-                    if (output->template data<float>()[i] > max[j]) {
-                        for (auto _j = k - 1; _j > j; --_j) {
-                            max[_j - 1] = max[_j];
-                            max_index[_j - 1] = max_index[_j];
-                        }
-                        max[j] = output->template data<float>()[i];
-                        max_index[j] = i;
-                        goto skip;
-                    }
+    auto data = output.data<float>();
+    for (auto i = 0; i < 1000; ++i) {
+        for (auto j = 0; j < k; ++j) {
+            if (data[i] > max[j]) {
+                for (auto _j = k - 1; _j > j; --_j) {
+                    max[_j] = max[_j - 1];
+                    max_index[_j] = max_index[_j - 1];
                 }
-                skip:;
+                max[j] = data[i];
+                max_index[j] = i;
+                break;
             }
         }
     }
+
     std::ostringstream stringStream;
     stringStream << avg_fps << " FPS\n";
+    //stringStream << output.slice(/*dim=*/1, /*start=*/0, /*end=*/5) << "\n";
 
     for (auto j = 0; j < k; ++j) {
-        stringStream << j << ": " << imagenet_classes[max_index[j]] << " - " << max[j] / 10 << "%\n";
+        stringStream << /*j << ": " <<*/ imagenet_classes[max_index[j]]/* << " - " << max[j]*/ << "\n";
     }
+
     return env->NewStringUTF(stringStream.str().c_str());
 }
